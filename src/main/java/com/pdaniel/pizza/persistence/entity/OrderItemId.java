787ccda8a0afecd5db5a7a6fadfba4755dc900d0
Daniel <danie.p.usb@gmail.com>
2024-01-06 03:03:03 +0100

    refactor: use lombok @EqualsAndHashCode instead of build one myself. OrderItemId.java File

--- src/main/java/com/pdaniel/pizza/persistence/entity/OrderItemId.java
+++ src/main/java/com/pdaniel/pizza/persistence/entity/OrderItemId.java
@@ -1,32 +1,15 @@
 package com.pdaniel.pizza.persistence.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class OrderItemId implements Serializable {
     private Integer idOrder;
     private Integer idItem;
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderItemId that = (OrderItemId) o;
-        return Objects.equals(idOrder, that.idOrder) && Objects.equals(idItem, that.idItem);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idOrder, idItem);
-    }
 }
